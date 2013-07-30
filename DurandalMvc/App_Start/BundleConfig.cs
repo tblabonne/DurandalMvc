06f9b80f1cedfa0048eaa8b8f8193a955634a8bf
tblabonne <tblabonne@gmail.com>
2013-07-30 12:52:21 -0500

    Initial commit

---
+++ DurandalMvc/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DurandalMvc
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/knockout-{version}.js",
+                        "~/Scripts/sammy-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/durandal.css",
+                "~/Content/site.css"
+                ));
+        }
+    }
+}
---
+++ DurandalMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DurandalMvc.MvcApplication" Language="C#" %>
